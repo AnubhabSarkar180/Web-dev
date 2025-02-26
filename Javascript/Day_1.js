39eee5fbbd2aa07bde801730d733c5a0bc2f279b
AnubhabSarkar180 <anubhabsarkar023@gmail.com>
2025-02-26 19:18:25 +0530

    Homework

---
+++ Javascript/Day_1.js
@@ -0,0 +1,34 @@
+// Exercise 1
+
+console.log("Welcome to Javascript!");
+alert("This is your first Javascript alert");
+document.write("Javascript is fun");
+
+
+// Exercise 2
+let fullName="Anubhab Sarkar";
+console.log(fullName);
+
+let age=18;
+console.log(age);
+
+let isStudent=true;
+console.log(isStudent);
+
+let favoriteColors=["Red", "Green", "Yellow" ];
+console.log(favoriteColors[1]);
+
+let person={name:"Anubhab Sarkar", age:18, city:"San Andreas"};
+console.log(person);
+
+
+// Exercise 3
+var int1=18;
+let int2=18;
+const int3=18;
+
+console.log(int1,int2,int3);
+
+const int3=19;
+console.log(int1,int2,int3);
+
---
+++ Javascript/Day_2.js
@@ -0,0 +1,31 @@
+// Exercise 4
+let num1=12;
+let num2=10;
+
+console.log(num1+num2);
+console.log(num1*num2);
+console.log(num1-num2);
+console.log(num1/num2);
+
+console.log(10==10);
+console.log(10===10);
+
+console.log(15%4);
+
+let x=10;
+let y=5;
+x=x+y;
+y=x-y;
+x=x-y;
+console.log(x," ",y);
+
+// Exercise 5
+let price=100;
+let quantity=10;
+console.log(price*quantity);
+
+
+// Bonus challenge
+let width=10;
+let height=12;
+console.log(width*height,"sq. units");
